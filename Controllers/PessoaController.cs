0e1f3987c49d25b4b548f8d1f9d2ca5101c1dad0
Ademilson Bertolin <ademilsonbertolinnn@gmail.com>
2021-09-28 19:54:41 -0300

    API final

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/pessoa")]
+    public class PessoaController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public PessoaController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //POST: api/pessoa/create
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create([FromBody] Pessoa pessoa)
+        {
+            _context.Pessoas.Add(pessoa);
+            _context.SaveChanges();
+            return Created("", pessoa);
+        }
+
+        //GET: api/pessoa/list
+        [HttpGet]
+        [Route("list")]
+        public IActionResult List() => Ok(_context.Pessoas.ToList());
+      
+    }
+}
